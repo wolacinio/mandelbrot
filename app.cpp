ce31900679e1c379436ee574dd86bf01da0121b0
Paweł Wolak <pwolak@opgk.rzeszow.pl>
2017-10-12 22:39:01 +0200

    added color

--- app.cpp
+++ app.cpp
@@ -13,6 +13,12 @@ struct complex
 	float imag;
 };
 
+struct color {
+	float red;
+	float blue;
+	float green;
+};
+
 const int width = 500;
 const int height = 500;
 Vertex view[width][height];
@@ -24,10 +30,52 @@ bool modul(complex);
 int calculate(complex);
 complex clear(complex);
 
+
+color colors[121];
+
+void HSV2RGB(float hue, float sat, float val, float &red, float &grn, float &blu)
+{
+	int i;
+	float f, p, q, t;
+	if(val==0) {red=0; grn=0; blu=0;}
+	else{
+		hue/=60;
+		i = (int)(hue);
+		f = hue-i;
+		p = val*(1-sat);
+		q = val*(1-(sat*f));
+		t = val*(1-(sat*(1-f)));
+		if (i==0) {red=val; grn=t; blu=p;}
+		else if (i==1) {red=q; grn=val; blu=p;}
+		else if (i==2) {red=p; grn=val; blu=t;}
+		else if (i==3) {red=p; grn=q; blu=val;}
+		else if (i==4) {red=t; grn=p; blu=val;}
+		else if (i==5) {red=val; grn=p; blu=q;}
+	}
+}
+ 
+//initialize array with colors used to color different levels
+void initializeColors()
+{
+	int level;
+	float r, g, b;
+	 
+	for (level=0; level<120; level++)
+	{
+		HSV2RGB(2.1*level, 0.85, 0.6, r, g, b);
+		//colors[level] = (TColor)(int)(r*255) + ( << 8) + ( << 16);
+		colors[level].red = (int)(r*255);
+		colors[level].blue = (int)(b*255);
+		colors[level].green = (int)(g*255);
+	}
+	//colors[120] = 0;
+}
+
+
 int main(int argc, char** argv){
 	cout << "Wprowadz ilosc iteracji: ";
-	//cin >> maxIter;
-	
+	cin >> maxIter;
+	initializeColors();
 	complex temp;
 	int color;
 
@@ -43,18 +91,16 @@ int main(int argc, char** argv){
 		temp.imag = i*ratioY + minY;
 		for (int j = 0; j < width; ++j) {
 			temp.real = j*ratioX + minX;
-			//color = calculate(temp);
+			color = calculate(temp);
 			// view[j][i].position = Vector2f(i, j);
 			// view[j][i].color = Color::Red;
 			fraktal[i*500+j].position = Vector2f(j, i);
-			if(j % 10 == 0)
-				fraktal[i*500+j].color = Color::Red;
-			else
-				fraktal[i*500+j].color = Color::Blue;
-			
-			//cout << color << " ";
+			//fraktal[i*500+j].color = Color::Red;
+			fraktal[i*500+j].color = Color(colors[color].red, colors[color].green, colors[color].blue);
+			// cout << color << " ";
+			cout << colors[color].red << endl;
 		}
-		//cout << endl;
+		// cout << endl;
 	}
 	
 	ContextSettings settings;
